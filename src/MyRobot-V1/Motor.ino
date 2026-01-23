812adb99bb692b2c3652432285f2bf8175621792
Brady <bradyspur@gmail.com>
2026-01-22 18:22:39 -0800

    added arrow key inputs

--- src/MyRobot-V1/Motor.ino
+++ src/MyRobot-V1/Motor.ino
@@ -8,7 +8,7 @@ int enB = 10;
 int in3 = 12;
 int in4 = 13;
 
-int speed = 200; // Default startup speed
+int speed = 200; // Default speed
 
 void setup() {
   pinMode(enA, OUTPUT); 
@@ -26,20 +26,23 @@ void loop() {
   if (Serial.available() > 0) {
     char cmd = Serial.read();
 
-    // Direction Controls
-    if (cmd == 'F')      drive(LOW, HIGH, LOW, HIGH);
-    else if (cmd == 'B') drive(HIGH, LOW, HIGH, LOW);
-    else if (cmd == 'R') drive(HIGH, LOW, LOW, HIGH);
-    else if (cmd == 'L') drive(LOW, HIGH, HIGH, LOW);
-    else if (cmd == 'S') drive(LOW, LOW, LOW, LOW);
-    // Speed Controls (0 - 9)
-    else if (cmd >= '0' && cmd <= '9') {
-      // Convert char '0'-'9' to integer 0-9
-      int val = cmd - '0';
-      
-      // Map 0-9 to PWM range 0-255
-      // We start mapping at 0, but practically motors stall < 60
-      speed = map(val, 0, 9, 0, 255); 
+   switch(cmd) {
+  case 'R': // Forward was turning left, fix by swapping motor directions
+    drive(HIGH, LOW, HIGH, LOW);  // Swap pins for forward
+    break;
+  case 'L': // Backward was turning right
+    drive(LOW, HIGH, LOW, HIGH);
+    break;
+  case 'B': // Left was moving forward
+    drive(LOW, HIGH, HIGH, LOW);
+    break;
+  case 'F': // Right was moving back
+    drive(HIGH, LOW, LOW, HIGH);
+    break;
+  case 'S': // Stop
+  default:
+    drive(LOW, LOW, LOW, LOW);
+    break;
     }
   }
 }
@@ -47,12 +50,9 @@ void loop() {
 void drive(int a1, int a2, int b1, int b2) {
   digitalWrite(in1, a1);
   digitalWrite(in2, a2);
-  analogWrite(enA, (a1 == a2) ? 0 : speed); 
+  analogWrite(enA, (a1 == a2) ? 0 : speed);
 
   digitalWrite(in3, b1);
   digitalWrite(in4, b2);
   analogWrite(enB, (b1 == b2) ? 0 : speed);
 }
-
-// hello
-// this is a new comment
